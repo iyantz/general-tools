618eb9c9f1a5b3732a3fd2e1d2f540838498361d
YANTONGZHOU <yantongzhou-wx@sinotrans.com>
2020-10-28 16:18:20 +0800

    新增忽略文件和提交测试文件

---
+++ src/test/java/cn/yantz/xmltools/XmlReaderUtilsTest.java
@@ -0,0 +1,13 @@
+package cn.yantz.xmltools;
+
+import junit.framework.TestCase;
+
+/**
+ * @author yantz@gillion.com.cn
+ * @version 1.0
+ * @link GeneralTools cn.yantz.xmltools
+ * @since 2020/10/28 15:23
+ */
+public class XmlReaderUtilsTest extends TestCase {
+
+}
